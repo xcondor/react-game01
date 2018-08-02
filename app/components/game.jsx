878afa82cf3fd82b665f0a59a1573b64a26047cf
xingyucheng <xingyucheng@jd.com>
2018-08-02 19:20:21 +0800

    修改五子棋

--- app/components/game.jsx
+++ app/components/game.jsx
@@ -6,7 +6,7 @@ class Game extends Component{
         super(props);
         this.state = {
             history: [{
-                squares: Array(9).fill(null)
+                squares: Array(25).fill(null)
             }],
             xIsNext: true
         }
@@ -30,19 +30,33 @@ class Game extends Component{
 
     calculateWinner (squares) {
         //获胜所可能得到的数组
+        // const lines = [
+        //     [0, 1, 2],
+        //     [3, 4, 5],
+        //     [6, 7, 8],
+        //     [0, 3, 6],
+        //     [1, 4, 7],
+        //     [2, 5, 8],
+        //     [0, 4, 8],
+        //     [2, 4, 6],
+        // ];
         const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
+            [0,1,2,3,4],
+            [5,6,7,8,9],
+            [10,11,12,13,14],
+            [15,16,17,18,19],
+            [20,21,22,23,24],
+            [0,5,10,15,20],
+            [1,6,11,16,21],
+            [2,7,12,17,22],
+            [3,8,13,18,23],
+            [4,9,14,19,24],
+            [1,6,12,18,24],
+            [4,8,12,16,20]
         ];
         for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            const [a, b, c,d,e] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
                 return squares[a];
             }
         }
